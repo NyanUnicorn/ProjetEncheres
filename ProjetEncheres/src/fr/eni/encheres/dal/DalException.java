cff702183b5535ce0ac163405874ad3a86a628ad
predfern2019 <predfern2019@48SE36-6XKWH42.ad.campus-eni.fr>
2020-09-08 13:51:11 +0200

    Update abstract Generic DAO Add connection pool context

--- ProjetEncheres/src/fr/eni/encheres/dal/DalException.java
+++ ProjetEncheres/src/fr/eni/encheres/dal/DalException.java
@@ -2,4 +2,31 @@ package fr.eni.encheres.dal;
 
 public class DalException extends Exception {
 
+	public static final String DAL_CANNOT_OPEN_CONECTION = "Ne  peut pas ouvrir connexion";
+	public static final String DAL_NAMING_EXCEPTION = "Erreur de nommage";
+	
+	
+	
+	public DalException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public DalException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+	public DalException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+	public DalException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	public DalException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }

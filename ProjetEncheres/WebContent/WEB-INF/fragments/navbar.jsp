cc0882f783f598026a62206d982a7457ab8cc80c
predfern2019 <predfern2019@48SE36-6XKWH42.ad.campus-eni.fr>
2020-09-14 09:22:13 +0200

    Set ListesEnchers / Encheres as index

--- ProjetEncheres/WebContent/WEB-INF/fragments/navbar.jsp
+++ ProjetEncheres/WebContent/WEB-INF/fragments/navbar.jsp
@@ -1,6 +1,6 @@
 <nav class="navbar">
     <div class="nav-left">
-        <a href="">
+        <a href="${pageContext.request.contextPath}">
             <h3>
                 ENI - Enchères
             </h3>
--- ProjetEncheres/src/fr/eni/encheres/servlet/ListeEncheres.java
+++ ProjetEncheres/src/fr/eni/encheres/servlet/ListeEncheres.java
@@ -20,7 +20,7 @@ import fr.eni.encheres.bo.Enchere;
 /**
  * Servlet implementation class ListeEncheres
  */
-@WebServlet("/ListeEncheres")
+@WebServlet("/Encheres")
 public class ListeEncheres extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
